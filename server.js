49e834b423dd0997f29984c309082ea6234308f1
Prabhat Tiwari <prabhattiwari20041@gmail.com>
2023-06-02 00:35:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = process.env.port || 8080;
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: "sql10.freesqldatabase.com",
+  user: "sql10622868",
+  password: "vhvbzjpT2C",
+  database: "sql10622868",
+});
+
+app.post("/adddata", function (req, res) {
+  const body = req.body;
+  console.log(body);
+  const sql_insert = `INSERT INTO Data (title, description, image, author, date, country) VALUES ("${body.title}","${body.description}","${body.image}","${body.author}","${body.date}","${body.country}");`;
+  db.query(sql_insert, (err) => {
+    if (err) {
+      return res.status(400).send("No able to create the user!");
+    } else {
+      res.status(200).send("Data added successfully!");
+    }
+  });
+});
+
+app.get("/getdata/:limit", function (req, res) {
+  const limit = parseInt(req.params.limit);
+  const sql = "SELECT * FROM Data ORDER BY id LIMIT ?";
+  db.query(sql, [limit], (err, result) => {
+    if (err) {
+      return res.status(500).send({ error: err.message });
+    }
+    res.status(200).json({
+      message: "success",
+      data: result,
+    });
+  });
+});
+
+app.listen(port, function () {
+  console.log("server running on 8080");
+});
